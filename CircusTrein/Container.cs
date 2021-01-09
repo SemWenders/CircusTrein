df3da3b2267e7370416b8d58ecdc1b785bb94d48
SemWenders <66679237+SemWenders@users.noreply.github.com>
2021-01-09 16:50:13 +0100

    made the project more OO

--- CircusTrein/Container.cs
+++ CircusTrein/Container.cs
@@ -22,5 +22,45 @@ namespace CircusTrein
             animals.Add(animal);
             Weight += animal.Size;
         }
+
+        public bool IsAnimalAble(Animal animal)
+        {
+            if (Weight + animal.Size <= 10)
+            {
+                foreach (var animalInContainer in animals)
+                {
+                    if (animal.AnimalType == AnimalType.Meat)
+                    {
+                        //there can only be one meat eating animal in a container
+                        if (animalInContainer.AnimalType == AnimalType.Meat)
+                        {
+                            return false;
+                        }
+
+                        else if (animalInContainer.AnimalType == AnimalType.Plants && animal.Size >= animalInContainer.Size)
+                        {
+                            //all the other animals in the container have to be bigger than the new meat eating animal
+                            return false;
+                        }
+                    }
+
+                    else if (animal.AnimalType == AnimalType.Plants)
+                    {
+                        if (animalInContainer.AnimalType == AnimalType.Meat && animalInContainer.Size >= animal.Size)
+                        {
+                            //the new animal would be eaten
+                            return false;
+                        }
+                    }
+                }
+                //if the animal meets all the requirements, return true
+                return true;
+            }
+            else
+            {
+                //the animal doesn't fit
+                return false;
+            }
+        }
     }
 }
--- CircusTrein/Train.cs
+++ CircusTrein/Train.cs
@@ -34,42 +34,10 @@ namespace CircusTrein
         {
             foreach (var container in containers)
             {
-                if (container.Weight + animal.Size <= 10)
+                if (container.IsAnimalAble(animal))
                 {
-                    bool addAnimalToContainer = true;
-                    foreach (var animalInContainer in container.animals)
-                    {
-                        if (animal.AnimalType == AnimalType.Meat)
-                        {
-                            //if the new animal is a meat eater, all other animals must be plant eaters and need to be bigger
-                            if (animalInContainer.AnimalType == AnimalType.Meat)
-                            {
-                                //only one meat-eating animal is allowed in a container
-                                addAnimalToContainer = false;
-                            }
-                            if (animalInContainer.AnimalType == AnimalType.Plants && animalInContainer.Size <= animal.Size)
-                            {
-                                //if there's an animal in the container smaller or equal the size of the new animal, it will get eaten, so go to the next container
-                                addAnimalToContainer = false;
-                            }
-                        }
-
-                        //if the new animal is a plant eater, it should be bigger than any meat eating animals in the container (if there are any)
-                        else if (animal.AnimalType == AnimalType.Plants)
-                        {
-                            if (animalInContainer.AnimalType == AnimalType.Meat && animalInContainer.Size >= animal.Size)
-                            {
-                                //if there's a meat-eating animal in the container, that's bigger or the same size as the new animal, go to the next container
-                                addAnimalToContainer = false;
-                            }
-                        }
-                    }
-                    if (addAnimalToContainer)
-                    {
-                        //if the new animal meets the requirements, add it to the container and exit the method
-                        container.AddAnimal(animal);
-                        return;
-                    }
+                    container.AddAnimal(animal);
+                    return;
                 }
             }
 
@@ -77,7 +45,6 @@ namespace CircusTrein
             Container newContainer = new Container();
             containers.Add(newContainer);
             newContainer.AddAnimal(animal);
-            return;
         }
 
         public void AddAnimals(List<Animal> animals)
